289c369810417bf239656c38ddf47455eb6a769c
Hieu Lai Minh <hieu.laiminh@nashtechglobal.com>
2023-06-27 17:44:53 +0700

    remove unused code

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { BookRoutingModule } from './book-routing.module';
 import { AddBookComponent } from './add-book/add-book.component';
 import { ListBookComponent } from './list-book/list-book.component';
-import { DetailBookComponent } from './detail-book/detail-book.component';
 import { SharedModule } from '../shared/shared.module';
 import { MatTableModule } from '@angular/material/table';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -12,7 +11,12 @@ import { BookFormComponent } from './_component/book-form/book-form.component';
 import { EditBookComponent } from './edit-book/edit-book.component';
 
 @NgModule({
-  declarations: [AddBookComponent, ListBookComponent, DetailBookComponent, BookFormComponent, EditBookComponent],
+  declarations: [
+    AddBookComponent,
+    ListBookComponent,
+    BookFormComponent,
+    EditBookComponent,
+  ],
   imports: [
     CommonModule,
     BookRoutingModule,
